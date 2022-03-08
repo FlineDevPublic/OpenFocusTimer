7375161fdd8bc2c58340897252b6c6f2f89782b0
Cihat Gündüz <commits@cihatguenduez.de>
2022-03-08 23:11:17 +0100

    Simplify & re-sort entries in Package.swift manifest file

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
   name: "OpenFocusTimer",
   defaultLocalization: "en",
   platforms: [.macOS(.v12), .iOS(.v15)],
-
   products: [
     .library(name: "AppEntryPoint", targets: ["AppEntryPoint"]),
     .library(name: "Model", targets: ["Model"]),
@@ -25,14 +22,14 @@ let package = Package(
     // Handy SwiftUI features that didn't make it into the SwiftUI (yet).
     .package(url: "https://github.com/Flinesoft/HandySwiftUI", .branch("main")),
 
+    // ⏰ A few schedulers that make working with Combine more testable and more versatile.
+    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.5.3"),
+
     // A library for building applications in a consistent and understandable way, with composition, testing, and ergonomics in mind.
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.33.1"),
 
     // Safely access Apple's SF Symbols using static typing Topics
     .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "2.1.3"),
-
-    // ⏰ A few schedulers that make working with Combine more testable and more versatile.
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.5.3"),
   ],
   targets: [
     .target(
@@ -58,10 +55,6 @@ let package = Package(
         .process("Model.xcdatamodeld")
       ]
     ),
-    .testTarget(
-      name: "ModelTests",
-      dependencies: ["Model"]
-    ),
     .target(
       name: "TimerFeature",
       dependencies: [
@@ -72,13 +65,6 @@ let package = Package(
         "Resources",
         .product(name: "SFSafeSymbols", package: "SFSafeSymbols"),
         "Utility",
-      ]
-    ),
-    .testTarget(
-      name: "TimerFeatureTests",
-      dependencies: [
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-        "TimerFeature",
       ]
     ),
     .target(
@@ -102,6 +88,17 @@ let package = Package(
       dependencies: [
         .product(name: "CombineSchedulers", package: "combine-schedulers"),
         "Model",
+      ]
+    ),
+    .testTarget(
+      name: "ModelTests",
+      dependencies: ["Model"]
+    ),
+    .testTarget(
+      name: "TimerFeatureTests",
+      dependencies: [
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+        "TimerFeature",
       ]
     ),
   ]
