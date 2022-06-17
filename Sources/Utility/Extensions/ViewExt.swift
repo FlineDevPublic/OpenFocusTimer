0ced14f058162ae5946106b82c7daed2f0bc5ecd
Cihat Gündüz <commits.public@fline.dev>
2022-06-17 16:53:29 +0200

    Upgrade to Xcode 14 + merge multiple targets into one

--- Sources/Utility/Extensions/ViewExt.swift
+++ Sources/Utility/Extensions/ViewExt.swift
@@ -5,29 +5,16 @@ import SwiftUI
       public func previewVariants() -> some View {
          Group {
             self
-               .previewDevice("Mac")
-               .preferredColorScheme(.light)
-               .environment(\.dynamicTypeSize, DynamicTypeSize.medium)
-               .previewDisplayName("Mac: Light mode, Medium")
-
-            #warning(#"🧑‍💻 macOS doesn't seem to support dynamic type – remove the `.dynamicTypeSize` from here?"#)
-            self
-               .previewDevice("Mac")
-               .preferredColorScheme(.dark)
-               .environment(\.dynamicTypeSize, DynamicTypeSize.xxxLarge)
-               .previewDisplayName("Mac: Dark mode, XXXLarge")
-
-            self
                .previewDevice("iPhone 13")
-               .preferredColorScheme(.light)
-               .environment(\.dynamicTypeSize, DynamicTypeSize.medium)
-               .previewDisplayName("iPhone 13: Light mode, Medium")
+               .previewDisplayName("iPhone 13")
 
             self
                .previewDevice("iPhone 8")
-               .preferredColorScheme(.dark)
-               .environment(\.dynamicTypeSize, DynamicTypeSize.xxxLarge)
-               .previewDisplayName("iPhone 8: Dark mode, XXXLarge")
+               .previewDisplayName("iPhone 8")
+
+            self
+               .previewDevice("Mac")
+               .previewDisplayName("Mac")
          }
       }
    }
